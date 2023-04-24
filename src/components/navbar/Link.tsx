8d035374e9db78c6dc5c3b8e4b951b9868197dab
mo7mad4 <mohammedreda814@gmail.com>
2023-04-24 12:24:47 +0200

    completed navbar and make hooks useMediaQuery

--- src/components/navbar/Link.tsx
+++ src/components/navbar/Link.tsx
@@ -1,14 +1,15 @@
+import { SelectedPage } from "@/shared/types";
 import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
     page : string,
-    selectedPage :string,
-    setSelectedPage: (value: string) => void;
+    selectedPage :SelectedPage,
+    setSelectedPage: (value: SelectedPage) => void;
 }
 
 const Link = ({page,selectedPage,setSelectedPage}: Props) => {
     // when i write id="myhome" the replace / /g , "" will remove any space between it example (my home) this wrong and the replace (myhome)
-    const lowerCasePage = page.toLowerCase().replace(/ /g,"")
+    const lowerCasePage = page.toLowerCase().replace(/ /g,"") as SelectedPage;
   return (
     <AnchorLink
         className={`${selectedPage === lowerCasePage ? "text-primary-500": ""} transition duration-500 hover:text-primary-300 `}
