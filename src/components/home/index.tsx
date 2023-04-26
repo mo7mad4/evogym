b2be12ec1543bc9aaf40d8a85c940e5dce7503aa
mo7mad4 <mohammedreda814@gmail.com>
2023-04-26 12:59:10 +0200

    UpLoad code Home component

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -6,6 +6,7 @@ import HomePageGraphic from '@/assets/HomePageGraphic.png'
 import SponsorRedBull from '@/assets/SponsorRedBull.png'
 import SponsorForbes from '@/assets/SponsorForbes.png'
 import SponsorFortune from '@/assets/SponsorFortune.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -19,33 +20,52 @@ const Home = ({ setSelectedPage }: Props) => {
             className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
         >
             {/* Image main header */}
-            <div>
-                {/* Main header */}
-                <div>
+            <div className='md:flex mx-auto w-5/6  items-center justify-center md:h-5/6'>
+                {/* Main header */} 
+                <div className='z-10 mt-32 md:basis-3/5'>
                     {/* Headings */}
-                    <div>
-                        <div>
-                            <div>
+                    <div className='md:-mt-20'>
+                        <div className='relative'>
+                            <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                                 <img src={HomePageText} alt="home-page-text" />
                             </div>
                         </div>
-                        <p>Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+                        <p className='mt-8 text-sm '>Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
                             Studios to get the Body Shapes That you Dream of.. Get Your Dream
                             Body Now.</p>
                     </div>
                     {/* Actions */}
-                    <div>
-                        <ActionButton 
-                            setSelectedPage={setSelectedPage}
+                    <div className='mt-8 flex items-center gap-8 '>
+                        <ActionButton setSelectedPage={setSelectedPage}> Join Now </ActionButton>
+                        <AnchorLink 
+                            className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+                            onClick={()=> setSelectedPage(SelectedPage.ContactUs)}
+                            href={`#${SelectedPage.ContactUs}`}
                         >
-                            Join Now
-                        </ActionButton>
+                            <p>Learn More</p>
+
+                        </AnchorLink>
                     </div>
                 </div>
 
                 {/* Image */}
-                <div></div>
+                <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:ml-16 md:justify-items-end '>
+                    <img src={HomePageGraphic} alt="home-PageGraphic" />
+                </div>
             </div>
+
+            {/* Sponsors */}
+            {isAboveMediumScreens && (
+                <div className='h-[150px] w-full bg-primary-100 py-10'>
+                    <div className='mx-auto w-5/6 '>
+                        <div className='flex w-3/5 items-center justify-between gap-8'>
+                            <img src={SponsorRedBull} alt="redbull-sponsors" />
+                            <img src={SponsorForbes} alt="forbes-sponsors" />
+                            <img src={SponsorFortune} alt="forrune-sponsors" />
+                        </div>
+                    </div>
+                </div>
+            )}
 
         </section>
     )
