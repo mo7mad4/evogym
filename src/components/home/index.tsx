e1672fe13553cc5df616f92c7e8ee55b91e2bd59
mo7mad4 <mohammedreda814@gmail.com>
2023-04-27 13:29:28 +0200

    complate Home component and make motion this section

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,4 +1,5 @@
 import { SelectedPage } from '@/shared/types'
+import { motion } from 'framer-motion'
 import useMediaQuery from '@/hooks/useMediaQuery'
 import ActionButton from '@/shared/ActionButton'
 import HomePageText from '@/assets/HomePageText.png'
@@ -20,11 +21,32 @@ const Home = ({ setSelectedPage }: Props) => {
             className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
         >
             {/* Image main header */}
-            <div className='md:flex mx-auto w-5/6  items-center justify-center md:h-5/6'>
+            <motion.div 
+                className='md:flex mx-auto w-5/6  items-center justify-center md:h-5/6'
+                onViewportEnter={()=> setSelectedPage(SelectedPage.Home)}
+                initial="hidden"
+                        whileInView="visible"
+                        viewport={{once:true, amount:0.5}}
+                        transition={{duration:1}}
+                        variants={{
+                            hidden:{opacity:0, x:-50 },
+                            visible:{opacity:1, x:0}
+                        }}
+            >
                 {/* Main header */} 
                 <div className='z-10 mt-32 md:basis-3/5'>
                     {/* Headings */}
-                    <div className='md:-mt-20'>
+                    <motion.div 
+                        className='md:-mt-20'
+                        whileInView="visible"
+                        initial="hidden"
+                        viewport={{once:true, amount:0.5}}
+                        transition={{duration:1}}
+                        variants={{
+                            hidden:{opacity:0, x:-50 },
+                            visible:{opacity:1, x:0}
+                        }}
+                    >
                         <div className='relative'>
                             <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                                 <img src={HomePageText} alt="home-page-text" />
@@ -33,9 +55,19 @@ const Home = ({ setSelectedPage }: Props) => {
                         <p className='mt-8 text-sm '>Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
                             Studios to get the Body Shapes That you Dream of.. Get Your Dream
                             Body Now.</p>
-                    </div>
+                    </motion.div>
                     {/* Actions */}
-                    <div className='mt-8 flex items-center gap-8 '>
+                    <motion.div 
+                        className='mt-8 flex items-center gap-8 '
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{once:true, amount:0.5}}
+                        transition={{duration:1, delay:0.5}}
+                        variants={{
+                            hidden:{opacity:0, x:-50 },
+                            visible:{opacity:1, x:0}
+                        }}
+                        >
                         <ActionButton setSelectedPage={setSelectedPage}> Join Now </ActionButton>
                         <AnchorLink 
                             className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
@@ -45,14 +77,14 @@ const Home = ({ setSelectedPage }: Props) => {
                             <p>Learn More</p>
 
                         </AnchorLink>
-                    </div>
+                    </motion.div>
                 </div>
 
                 {/* Image */}
                 <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:ml-16 md:justify-items-end '>
                     <img src={HomePageGraphic} alt="home-PageGraphic" />
                 </div>
-            </div>
+            </motion.div>
 
             {/* Sponsors */}
             {isAboveMediumScreens && (
